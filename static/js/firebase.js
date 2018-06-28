3c6735aa6f5e5d495595c8dd5c24251d9599ebd1
tjarbo <16938041+tjarbo@users.noreply.github.com>
2018-06-28 23:35:17 +0200

    Vorlage erstellt

---
+++ static/js/firebase.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+  apiKey: "anj324nhji2r3u24n34nc2234bj34",
+  authDomain: "projekt-1234.firebaseapp.com",
+  databaseURL: "https://projekt-1234.firebaseio.com",
+  projectId: "projekt-1234",
+  storageBucket: "projekt-1234.appspot.com",
+  messagingSenderId: "1234567890"
+};
+
+var owm_apikey = << YOUR OPENWEATHERMAP API-KEY - SEE NEXT STEP >>;
+
+firebase.initializeApp(config);
